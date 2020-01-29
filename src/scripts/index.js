5df4542f23a9880cf1240a90fe01d7e7ccba74aa
Ben Anderson <ben.anderson@wanews.com.au>
2020-01-29 16:25:39 +0800

    Change API format to XML

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,16 +2,19 @@ import '../style.scss';
 import { createTable, tableData, createButton } from './ladderFuncs';
 import thisYear from './yearFunc';
 
+const parser = require('fast-xml-parser');
+
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 document.body.appendChild(container);
 
 const request = new XMLHttpRequest();
-request.open('GET', 'https://api.squiggle.com.au/?q=standings', true);
+request.open('GET', 'https://api.squiggle.com.au/?q=standings;format=xml', true);
 
 request.onload = function apiData() {
-  const data = JSON.parse(this.response).standings;
+  const jsonObject = parser.parse(this.response);
+  const data = jsonObject.standings.anon;
 
   if (request.status >= 200 && request.status < 400) {
     const title = document.createElement('h1');
