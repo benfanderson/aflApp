32106fc0b125fd6629db4d3aadc8a20a6451cff3
Ben Anderson <ben.anderson@wanews.com.au>
2020-02-03 07:56:06 +0800

    Finalised inline notes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,17 +11,12 @@ container.setAttribute('class', 'container');
 document.body.appendChild(container);
 
 const request = new XMLHttpRequest();
-//   For Champion Data version: replace URL below with Champion Data version
+//   Replace URL below with Champion Data version
 request.open('GET', 'https://api.squiggle.com.au/?q=standings;format=xml', true);
 
 request.onload = function apiData() {
-  const jsonObject = parser.parse(this.response);
-  const data = jsonObject.standings.anon;
-
-  //   For Champion Data version: replace the two lines above with two lines below
-
-  //   const jsonObject = parser.parse(this.response).report.ladder;
-  //   const data = Object.values(xmlData[0].squad);
+  const jsonObject = parser.parse(this.response).report.ladder;
+  const data = Object.values(jsonObject[0].squad);
 
   if (request.status >= 200 && request.status < 400) {
     const title = document.createElement('h1');
--- src/scripts/ladderFuncs.js
+++ src/scripts/ladderFuncs.js
@@ -50,58 +50,46 @@ export function createTable() {
 export function tableData(data) {
   const aflTable = document.body.children[1].children[2];
   data.forEach((team) => {
-    // For Champion Data version: Add line below and replace selected lines with noted lines below
-    // const teamInfo = Object.values(team)[0];
+    const teamInfo = Object.values(team)[0];
     const tableRow = document.createElement('tr');
     aflTable.appendChild(tableRow);
     const rank = document.createElement('td');
     tableRow.appendChild(rank);
-    rank.innerHTML = `${team.rank}. `; // Replace with below
-    // rank.innerHTML = `${teamInfo.position}. `;
+    rank.innerHTML = `${teamInfo.position}. `;
     const logo = document.createElement('td');
     tableRow.appendChild(logo);
     const logoImg = document.createElement('img');
     logo.appendChild(logoImg);
     logoImg.setAttribute('class', 'clubLogo');
-    logoImg.src = createImgSrc(team.name); // Replace with below
-    // logoImg.src = createImgSrc(teamInfo.squadName);
+    logoImg.src = createImgSrc(teamInfo.squadName);
     const club = document.createElement('td');
     tableRow.appendChild(club);
     club.setAttribute('class', 'clubName');
-    club.innerHTML = team.name; // Replace with below
-    // club.innerHTML = teamInfo.squadName;
+    club.innerHTML = teamInfo.squadName;
     const played = document.createElement('td');
     tableRow.appendChild(played);
-    played.innerHTML = team.played; // Replace with below
-    // played.innerHTML = teamInfo.played;
+    played.innerHTML = teamInfo.played;
     const wins = document.createElement('td');
     tableRow.appendChild(wins);
-    wins.innerHTML = team.wins; // Replace with below
-    // wins.innerHTML = teamInfo.win;
+    wins.innerHTML = teamInfo.win;
     const losses = document.createElement('td');
     tableRow.appendChild(losses);
-    losses.innerHTML = team.losses; // Replace with below
-    // losses.innerHTML = teamInfo.loss;
+    losses.innerHTML = teamInfo.loss;
     const draws = document.createElement('td');
     tableRow.appendChild(draws);
-    draws.innerHTML = team.draws;
-    // draws.innerHTML = teamInfo.drawn;
+    draws.innerHTML = teamInfo.drawn;
     const pointsFor = document.createElement('td');
     tableRow.appendChild(pointsFor);
-    pointsFor.innerHTML = team.for; // Replace with below
-    // pointsFor.innerHTML = teamInfo.ptsFor;
+    pointsFor.innerHTML = teamInfo.ptsFor;
     const pointsAgst = document.createElement('td');
     tableRow.appendChild(pointsAgst);
-    pointsAgst.innerHTML = team.against; // Replace with below
-    // pointsAgst.innerHTML = teamInfo.ptsAgainst;
+    pointsAgst.innerHTML = teamInfo.ptsAgainst;
     const percentage = document.createElement('td');
     tableRow.appendChild(percentage);
-    percentage.innerHTML = team.percentage; // Replace with below
-    // percentage.innerHTML = teamInfo.percentage;
+    percentage.innerHTML = teamInfo.percentage;
     const points = document.createElement('td');
     tableRow.appendChild(points);
-    points.innerHTML = team.pts; // Replace with below
-    // points.innerHTML = teamInfo.points;
+    points.innerHTML = teamInfo.points;
     addExtendedClass(played, draws, pointsFor, pointsAgst, percentage);
   });
 }
