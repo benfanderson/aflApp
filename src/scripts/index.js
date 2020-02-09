061043424b5b5b895e9281699ebcae741e4a14c6
Ben Anderson <ben.anderson@wanews.com.au>
2020-02-09 10:45:31 +0800

    More accurate variable names in index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,14 +11,14 @@ document.body.appendChild(container);
 
 fetch('https://api.squiggle.com.au/?q=standings;format=xml')
   .then((resp) => resp.text())
-  .then((data) => {
-    const jsonObject = parser.parse(data).standings.anon;
+  .then((jsonObject) => {
+    const data = parser.parse(jsonObject).standings.anon;
     const title = document.createElement('h1');
     container.appendChild(title);
     title.innerHTML = `${thisYear()} AFL Premiership Season`;
     createButton();
     createTable();
-    tableData(jsonObject);
+    tableData(data);
   })
   .catch(() => {
     const errorMessage = document.createElement('div');
